3c1aeb836e7d454ba45b4144a907a8dad99be366
sonysn <stephen.nyamali@gmail.com>
2022-11-25 10:08:39 +0100

    initial commit

---
+++ lib/time_greeting.dart
@@ -0,0 +1,37 @@
+library time_greeting;
+import 'package:flutter/material.dart';
+
+
+//Returns a String based on the current time.
+String getTimeString() {
+  final now = DateTime.now();
+  int hour = now.hour;
+  String n = "";
+  if ((hour >= 0) & (hour <= 11)) {
+    n = "Good Morning,";
+  } else if ((hour >= 12) & (hour <= 17)) {
+    n = "Good Afternoon,";
+  } else if ((hour >= 18) & (hour <= 23)) {
+    n = "Good Evening,";
+  }
+  return n;
+}
+
+//Returns an Icon and Colour based on the current time, Used for the "Good morning" message and the icon beside it.
+dynamic getTimeIconAndColour () {
+  final now = DateTime.now();
+  int hour = now.hour;
+  IconData n = Icons.cloud;
+  Color f = Colors.grey.shade300;
+  if ((hour >= 0) & (hour <= 6)) {
+    n = Icons.bedtime_rounded;
+    f = Colors.grey.shade300;
+  } else if ((hour >= 7) & (hour <= 18)) {
+    n = Icons.wb_sunny;
+    f = Colors.yellow;
+  } else if ((hour >= 19) & (hour <= 23)) {
+    n = Icons.bedtime_rounded;
+    f = Colors.grey.shade300;
+  }
+  return [n, f];
+}
---
+++ test/time_greeting_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:time_greeting/time_greeting.dart';
+
+void main() {
+}
